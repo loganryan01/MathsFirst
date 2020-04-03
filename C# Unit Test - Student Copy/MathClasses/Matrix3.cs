9a30ffd0f5a5b37417ee7d898402a9a19c4007d6
Logan Ryan <loganryan982@gmail.com>
2020-04-03 14:39:00 +1100

    Created a second matrix3 constructor. This constructor contains parameters.

--- C# Unit Test - Student Copy/MathClasses/Matrix3.cs
+++ C# Unit Test - Student Copy/MathClasses/Matrix3.cs
@@ -17,5 +17,11 @@ namespace MathClasses
             m7 = 0; m8 = 0; m9 = 1;
         }
 
+        public Matrix3(float m1, float m2, float m3, float m4, float m5, float m6, float m7, float m8, float m9)
+        {
+            this.m1 = m1; this.m2 = m2; this.m3 = m3;
+            this.m4 = m4; this.m5 = m5; this.m6 = m6;
+            this.m7 = m7; this.m8 = m8; this.m9 = m9;
+        }
     }
 }

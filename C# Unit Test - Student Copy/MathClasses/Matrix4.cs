5317eac377a9a0353b63323121336163b03a4d48
Logan Ryan <loganryan982@gmail.com>
2020-04-03 15:58:20 +1100

    Created a second matrix4 constructor. This contains parameters.

--- C# Unit Test - Student Copy/MathClasses/Matrix4.cs
+++ C# Unit Test - Student Copy/MathClasses/Matrix4.cs
@@ -18,5 +18,12 @@ namespace MathClasses
             m13= 0; m14= 0; m15= 0; m16= 1;
         }
 
+        public Matrix4(float m1, float m2, float m3, float m4, float m5, float m6, float m7, float m8, float m9, float m10, float m11, float m12, float m13. float m14, float m15, float m16)
+        {
+            this.m1 = m1; this.m2 = m2; this.m3 = m3; this.m4 = m4;
+            this.m5 = m5; this.m6 = m6; this.m7 = m7; this.m8 = m8;
+            this.m9 = m9; this.m10 = m10; this.m11 = m11; this.m12 = m12;
+            this.m13 = m13; this.m14 = m14; this.m15 = m15; this.m16 = m16;
+        }
     }
 }

710ab50edaa81ef12d69684a8fdfbdca9f96097c
Logan Ryan <loganryan982@gmail.com>
2020-04-03 16:18:40 +1100

    Created a set function that will update the values of the 4x4 matrix to the values of the input matrix.

--- C# Unit Test - Student Copy/MathClasses/Matrix4.cs
+++ C# Unit Test - Student Copy/MathClasses/Matrix4.cs
@@ -55,5 +55,13 @@ namespace MathClasses
                 rhs.m13 * lhs.m3 + rhs.m14 * lhs.m7 + rhs.m15 * lhs.m11 + rhs.m16 * lhs.m15,
                 rhs.m13 * lhs.m4 + rhs.m14 * lhs.m8 + rhs.m15 * lhs.m12 + rhs.m16 * lhs.m16);
         }
+
+        public void Set(float m1, float m2, float m3, float m4, float m5, float m6, float m7, float m8, float m9, float m10, float m11, float m12, float m13, float m14, float m15, float m16)
+        {
+            this.m1 = m1; this.m2 = m2; this.m3 = m3; this.m4 = m4;
+            this.m5 = m5; this.m6 = m6; this.m7 = m7; this.m8 = m8;
+            this.m9 = m9; this.m10 = m10; this.m11 = m11; this.m12 = m12;
+            this.m13 = m13; this.m14 = m14; this.m15 = m15; this.m16 = m16;
+        }
     }
 }

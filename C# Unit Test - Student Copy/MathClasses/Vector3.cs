08bd61dabd987c60055b215b631d5d41014afcf4
Logan Ryan <loganryan982@gmail.com>
2020-04-03 16:29:59 +1100

    Created a second vector3 constructor. This has parameters.

--- C# Unit Test - Student Copy/MathClasses/Vector3.cs
+++ C# Unit Test - Student Copy/MathClasses/Vector3.cs
@@ -17,5 +17,11 @@ namespace MathClasses
             z = 0;
         }
 
+        public Vector3(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
     }
 }

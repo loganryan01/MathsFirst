f17dd3013c6e7b56692a316cf61274ed185c9a01
Logan Ryan <loganryan982@gmail.com>
2020-04-05 13:00:57 +1000

    Fixed the positioning of the turret on the tank.

--- RaylibStarterCS/Project2D/Game.cs
+++ RaylibStarterCS/Project2D/Game.cs
@@ -36,15 +36,15 @@ namespace Project2D
             stopwatch.Start();
             lastTime = stopwatch.ElapsedMilliseconds;
 
-            tankSprite.Load("tankBlue_outline.png");
+            tankSprite.Load(@"D:\\Windows\\PNG\\Tanks\\tankBlue_outline.png");
             // sprite is facing the wrong way... fix that here
             tankSprite.SetRotate(-90 * (float)(Math.PI / 180.0f));
             // sets an offset for the base, so it rotates around the centre
             tankSprite.SetPosition(-tankSprite.Width / 2.0f, tankSprite.Height / 2.0f);
-            turretSprite.Load("barrelBlue.png");
+            turretSprite.Load(@"D:\\Windows\\PNG\\Tanks\\barrelBlue.png");
             turretSprite.SetRotate(-90 * (float)(Math.PI / 180.0f));
             // set the turret offset from the tank base
-            turretSprite.SetPosition(0, turretSprite.Width / 2.0f);
+            turretSprite.SetPosition(-75, 75);
 
             // set up the scene object hierarchy - parent the turret to the base,
             // then the base to the tank sceneObject

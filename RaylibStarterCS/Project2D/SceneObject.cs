1103bebdc6a48f0cc6e03fc075066e1ac2277878
Logan Ryan <loganryan982@gmail.com>
2020-04-05 12:22:10 +1000

    Created a method that updates the global transform on all child objects.

--- RaylibStarterCS/Project2D/SceneObject.cs
+++ RaylibStarterCS/Project2D/SceneObject.cs
@@ -110,5 +110,16 @@ namespace Project2D
                 child.Draw();
             }
         }
+
+        public void UpdateTransform()
+        {
+            if (parent != null)
+                globalTransform = parent.globalTransform * localTransform;
+            else
+                globalTransform = localTransform;
+
+            foreach (SceneObject child in children)
+                child.UpdateTransform();
+        }
     }
 }

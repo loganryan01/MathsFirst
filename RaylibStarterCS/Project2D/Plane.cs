794a2e775f21a19e3bbb8205198335f4aa944aa2
Logan Ryan <loganryan982@gmail.com>
2020-04-07 11:18:33 +1000

    Created Plane class with constructors where the values are set directly, either by passing in a vector and a float, or by passing in 3 floats.

---
+++ RaylibStarterCS/Project2D/Plane.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathClasses;
+
+namespace Project2D
+{
+    // 2D mathematical plane using Ax + By + d = 0
+    class Plane
+    {
+        Vector3 N;
+        float d;
+
+        public Plane()
+        {
+
+        }
+
+        public Plane(float x, float y, float d)
+        {
+            this.N = new Vector3(x, y, 0);
+            this.d = d;
+        }
+    }
+}

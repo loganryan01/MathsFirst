f1af00690e9a9cf6f68e4bb81322bd58cdb0891d
Logan Ryan <loganryan982@gmail.com>
2020-04-08 17:51:56 +1000

    Started making quadrants in tank game to set up bullet path for tank.

--- RaylibStarterCS/Project2D/Game.cs
+++ RaylibStarterCS/Project2D/Game.cs
@@ -24,6 +24,9 @@ namespace Project2D
         static Vector3 min1 = new Vector3(0, 0, 1);
         static Vector3 min2 = new Vector3();
         static Vector3 max2 = new Vector3();
+        Vector3 direction1 = new Vector3();
+        Vector3 direction2 = new Vector3();
+        Vector3 origin = new Vector3();
 
         AABB tank = new AABB(min2, max2);
         AABB box = new AABB(min1, max1);
@@ -65,12 +68,6 @@ namespace Project2D
             // position/rotation of the tank without
             // affecting the offset of the base sprite
             tankObject.SetPosition(GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f);
-            //42, 42
-
-            //min2.x = tankSprite.LocalTransform.m7 - 42;
-            //min2.y = tankSprite.LocalTransform.m8 - 42;
-            //max2.x = tankSprite.LocalTransform.m7 + 42;
-            //max2.y = tankSprite.LocalTransform.m8 + 42;
         }
 
         public void Shutdown()
@@ -153,6 +150,17 @@ namespace Project2D
             max2.y = tankObject.LocalTransform.m8 + 50;
             max2.z = 1;
 
+            origin.x = tankObject.LocalTransform.m7;
+            origin.y = tankObject.LocalTransform.m8;
+            origin.z = 1;
+
+            
+
+            // m1 >= 0 || m2 <= 0: Lines at (640, 240) and (320, 0)
+            // m1 <= 0 || m2 <= 0: Lines at (320, 0) and (0, 240)
+            // m1 <= 0 || m2 >= 0: Lines at (0, 240) and (320, 480)
+            // m1 >= 0 || m2 >= 0: Lines at (320, 480) and (640, 240)
+
             BeginDrawing();
 
             ClearBackground(rl.Color.WHITE);
@@ -161,11 +169,52 @@ namespace Project2D
             DrawRectangleLines((int)(tankObject.LocalTransform.m7 - 52), (int)(tankObject.LocalTransform.m8 - 50), 100, 100, rl.Color.BLACK);
             DrawRectangleLines(0, 0, GetScreenWidth(), GetScreenHeight(), rl.Color.BLUE);
 
+            //DrawLine((int)(origin.x), (int)(origin.y), (int)(direction.x), (int)(direction.y), rl.Color.BLACK);
+
+            DrawText((turretObject.LocalTransform.m1).ToString(), 10, 30, 14, rl.Color.RED);
+            DrawText((turretObject.LocalTransform.m2).ToString(), 10, 50, 14, rl.Color.RED);
+
+            if (turretObject.LocalTransform.m1 >= 0 || turretObject.LocalTransform.m2 <= 0)
+            {
+                direction1.x = 640;
+                direction1.y = 240;
+                direction2.x = 320;
+                direction2.y = 0;
+                DrawLine((int)(origin.x), (int)(origin.y), (int)(direction1.x), (int)(direction1.y), rl.Color.BLACK);
+                DrawLine((int)(origin.x), (int)(origin.y), (int)(direction2.x), (int)(direction2.y), rl.Color.BLACK);
+            }
+            else if (turretObject.LocalTransform.m1 <= 0 || turretObject.LocalTransform.m2 <= 0)
+            {
+                direction1.x = 320;
+                direction1.y = 0;
+                direction2.x = 0;
+                direction2.y = 240;
+                DrawLine((int)(origin.x), (int)(origin.y), (int)(direction1.x), (int)(direction1.y), rl.Color.BLACK);
+                DrawLine((int)(origin.x), (int)(origin.y), (int)(direction2.x), (int)(direction2.y), rl.Color.BLACK);
+            }
+            else if (turretObject.LocalTransform.m1 <= 0 || turretObject.LocalTransform.m2 >= 0)
+            {
+                direction1.x = 0;
+                direction1.y = 240;
+                direction2.x = 320;
+                direction2.y = 480;
+                DrawLine((int)(origin.x), (int)(origin.y), (int)(direction1.x), (int)(direction1.y), rl.Color.BLACK);
+                DrawLine((int)(origin.x), (int)(origin.y), (int)(direction2.x), (int)(direction2.y), rl.Color.BLACK);
+            }
+            else if (turretObject.LocalTransform.m1 >= 0 || turretObject.LocalTransform.m2 >= 0)
+            {
+                direction1.x = 320;
+                direction1.y = 480;
+                direction2.x = 640;
+                direction2.y = 240;
+                DrawLine((int)(origin.x), (int)(origin.y), 320, 480, rl.Color.BLACK);
+                DrawLine((int)(origin.x), (int)(origin.y), 640, 240, rl.Color.BLACK);
+            }
             //if (tank.Overlaps(box))
             //{
             //    DrawText("Tank overlaps box", 10, 70, 14, rl.Color.RED);
             //}
-            //m1 = 1, when the front of the tank is facing left
+            //m1 = 1, when the front of the tank is facing right
             //m2 = 1, when the front of the tank is facing down
             //m3 is constantly 0.
             //m4 = 1, when the front of the tank is facing up.
@@ -174,7 +223,7 @@ namespace Project2D
             //m7 is x position of the tank.
             //m8 is y position of the tank.
             //m9 is constantly 1.
-            // turretObject.LocalTransform.m5, turretObject.LocalTransform.m2
+            // , turretObject.LocalTransform.m2
             //The width of the tank is 83.
             //The height of the tank is 78.
 

68df852ba176d307e463d8b0a6f532cd06483ec7
Logan Ryan <loganryan982@gmail.com>
2020-04-08 10:52:31 +1000

    Created a basic Ray class declaration and given the Ray 3 properties; and origin point, a direction vector that should always be normalised, and a scalar length value stored in a float. Created a constructor that allows me to pass in the values for the properties.

---
+++ RaylibStarterCS/Project2D/Ray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathClasses;
+
+namespace Project2D
+{
+    // ray where direction should be normalised
+    class Ray
+    {
+        Vector3 origin;
+        Vector3 direction;
+        float length;
+
+        public Ray()
+        {
+
+        }
+
+        public Ray(Vector3 start, Vector3 direction, float length = float.MaxValue)
+        {
+            this.origin = start;
+            this.direction = direction;
+            this.length = length;
+        }
+    }
+}

259e2dfd2f7d1559080a61dff0ec43f41f611ce0
Logan Ryan <loganryan982@gmail.com>
2020-04-09 16:12:59 +1000

    Created Sphere class to allow bullet to collide with objects.

---
+++ RaylibStarterCS/Project2D/Sphere.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathClasses;
+
+namespace Project2D
+{
+    class Sphere
+    {
+        Vector3 center;
+        float radius;
+
+        public Sphere()
+        {
+
+        }
+
+        public Sphere(Vector3 p, float r)
+        {
+            this.center = p;
+            this.radius = r;
+        }
+
+        public bool Overlaps (AABB aabb)
+        {
+            Vector3 diff = aabb.ClosestPoint(center) - center;
+            return diff.Dot(diff) <= (radius * radius);
+        }
+    }
+}
